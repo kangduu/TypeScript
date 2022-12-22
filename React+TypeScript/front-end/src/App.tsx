d32f1cd67b8076fd9d8ced47243ee69c9dc1b0b8
dukang <dk@nosugartech.com>
2022-12-22 11:14:22 +0800

    Fix：React+

---
+++ React+TypeScript/front-end/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Increment from "./components/AutomaticBatching";
+
+function App() {
+  return (
+    <div className="App">
+      <Increment />
+    </div>
+  );
+}
+
+export default App;
